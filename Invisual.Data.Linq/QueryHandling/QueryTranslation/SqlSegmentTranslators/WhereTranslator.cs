22409da5646e0925d2ee2a337648d93900bc6478
Tom Bowers <tom@invisual.co.uk>
2015-08-10 21:59:57 +0100

    Implemented where clause expression resolution.

--- Invisual.Data.Linq/QueryHandling/QueryTranslation/SqlSegmentTranslators/WhereTranslator.cs
+++ Invisual.Data.Linq/QueryHandling/QueryTranslation/SqlSegmentTranslators/WhereTranslator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -21,6 +22,56 @@ namespace Invisual.Data.Linq.QueryHandling.QueryTranslation.SqlSegmentTranslator
 		public string GetSql()
 		{
 			return _sql.ToString();
+		}
+
+		protected override Expression VisitConstant(ConstantExpression node)
+		{
+			var q = node.Value as IQueryable;
+			if (q != null)
+			{
+				// Assume constant nodes with IQueryables are table references
+				// TODO: Refine. Is this needed?
+				_sql.Append("SELECT * FROM ");
+				_sql.Append(q.ElementType.Name);
+			}
+			else if (node.Value == null)
+			{
+				_sql.Append("NULL");
+			}
+			else
+			{
+				switch (Type.GetTypeCode(node.Value.GetType()))
+				{
+					case TypeCode.Boolean:
+						_sql.Append(((bool)node.Value) ? 1 : 0);
+						break;
+
+					case TypeCode.String:
+						_sql.Append("'");
+						_sql.Append(node.Value);
+						_sql.Append("'");
+						break;
+
+					case TypeCode.Object:
+						throw new NotSupportedException(string.Format("The constant for '{0}' is not supported", node.Value));
+
+					default:
+						_sql.Append(node.Value);
+						break;
+				}
+			}
+
+			return node;
+		}
+
+		protected override Expression VisitMember(MemberExpression node)
+		{
+			if (node.Expression == null || node.Expression.NodeType != ExpressionType.Parameter)
+				throw new NotSupportedException(string.Format("The member '{0}' is not supported", node.Member.Name));
+
+			_sql.Append(node.Member.Name);
+
+			return node;
 		}
 
 		protected override Expression VisitBinary(BinaryExpression node)
@@ -72,6 +123,43 @@ namespace Invisual.Data.Linq.QueryHandling.QueryTranslation.SqlSegmentTranslator
 			_sql.Append(")");
 
 			return node;
+		}
+
+		protected override Expression VisitMethodCall(MethodCallExpression node)
+		{
+			if (node.Method.DeclaringType != typeof(Queryable) || node.Method.Name != "Where")
+				throw new NotSupportedException(string.Format("The method '{0}' is not supported", node.Method.Name));
+
+			_sql.AppendFormat("SELECT * FROM ({0}) AS T WHERE ", Visit(node.Arguments[0]));
+
+			var lambda = (LambdaExpression)StripQuotes(node.Arguments[1]);
+			Visit(lambda.Body);
+
+			return node;
+		}
+
+		//protected override Expression VisitUnary(UnaryExpression node)
+		//{
+		//	switch (node.NodeType)
+		//	{
+		//		case ExpressionType.Not:
+		//			_sql.Append(" NOT ");
+		//			Visit(node.Operand);
+		//			break;
+
+		//		//default:
+		//		//	throw new NotSupportedException(string.Format("The unary operator '{0}' is not supported", node.NodeType));
+		//	}
+
+		//	return node;
+		//}
+
+		private static Expression StripQuotes(Expression expression)
+		{
+			while (expression.NodeType == ExpressionType.Quote)
+				expression = ((UnaryExpression)expression).Operand;
+
+			return expression;
 		}
 	}
 }
--- Tester/Program.cs
+++ Tester/Program.cs
@@ -4,6 +4,7 @@ using Invisual.Data.SqlServer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,13 +14,31 @@ namespace Tester
 	{
 		static void Main(string[] args)
 		{
-			var table = new DatabaseTable<object>(new SqlServerDataSource("test"));
+			var db = new TestDatabase();
 
-			var test = table
-				.Where(x1 => x1 == new object())
-				.Where(x2 => x2 == new object())
+			var test = db.Table1
+				.Where(x => x.Id == "RT354DFGD")
+				.Where(x => x.Name == "TestName")
+				.OrderBy(x => x.Name)
 				.Take(10)
 				.ToList();
 		}
+	}
+
+	public class TestDatabase
+	{
+		public DatabaseTable<TestTable> Table1
+		{
+			get
+			{
+				return new DatabaseTable<TestTable>(new SqlServerDataSource("test"));
+			}
+		}
+	}
+
+	public class TestTable
+	{
+		public string Id { get; set; }
+		public string Name { get; set; }
 	}
 }
